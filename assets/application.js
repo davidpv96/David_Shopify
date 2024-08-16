7f4d5a755e5f57fc885292cc702acb569dea902f
David Peñavasquez <davidpv96@hotmail.com>
2024-08-16 03:13:09 -0500

    slider de textos en el hero

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,44 @@
 // Put your application javascript here
+document.addEventListener('DOMContentLoaded', function () {
+    function initSwiper(selector, config) {
+      return new Swiper(selector, config);
+    }
+  
+    // Slider existente
+    initSwiper(".mySwiper", {
+      slidesPerView: 1.2,
+      spaceBetween: 20,
+      breakpoints: {
+        640: {
+          slidesPerView: 1.5,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2.5,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 6,
+          spaceBetween: 20,
+        },
+      },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+      },
+    });
+  
+    // Nuevo slider (solo texto)
+    initSwiper(".myTextSwiper", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      autoplay: {
+        delay: 3000,
+      },
+    });
+  });

a19bfd7145953b6854473ba4f22077b77e4445a9
David Peñavasquez <davidpv96@hotmail.com>
2024-08-22 17:24:43 -0500

    agregando main content

--- assets/application.js
+++ assets/application.js
@@ -1,44 +1,65 @@
 // Put your application javascript here
 document.addEventListener('DOMContentLoaded', function () {
-    function initSwiper(selector, config) {
-      return new Swiper(selector, config);
-    }
-  
-    // Slider existente
-    initSwiper(".mySwiper", {
-      slidesPerView: 1.2,
-      spaceBetween: 20,
-      breakpoints: {
+  function initSwiper(selector, config) {
+    return new Swiper(selector, config);
+  }
+
+  // Slider existente
+  initSwiper(".mySwiper", {
+    slidesPerView: 1.2,
+    spaceBetween: 20,
+    breakpoints: {
+      640: {
+        slidesPerView: 1.5,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2.5,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 6,
+        spaceBetween: 20,
+      },
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: true,
+    },
+  });
+
+  // Nuevo slider (solo texto)
+  initSwiper(".myTextSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+    },
+  });
+
+  // Slider de testimonios
+  initSwiper(".myTestimonialsSwiper", {
+    slidesPerView: 1.5, // Muestra 1.5 slides en pantallas pequeñas
+    spaceBetween: 20, // Espacio entre slides
+    loop: true, // Para un deslizamiento infinito
+    breakpoints: {
         640: {
-          slidesPerView: 1.5,
-          spaceBetween: 20,
+            slidesPerView: 1.5, // 1.5 slides en pantallas pequeñas
+            spaceBetween: 20,
         },
         768: {
-          slidesPerView: 2.5,
-          spaceBetween: 20,
+            slidesPerView: 2.5, // 2.5 slides en pantallas medianas
+            spaceBetween: 20,
         },
         1024: {
-          slidesPerView: 6,
-          spaceBetween: 20,
+            slidesPerView: 3, // 3 slides en pantallas grandes
+            spaceBetween: 20,
         },
-      },
-      scrollbar: {
-        el: ".swiper-scrollbar",
-        hide: true,
-      },
-    });
-  
-    // Nuevo slider (solo texto)
-    initSwiper(".myTextSwiper", {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      autoplay: {
-        delay: 3000,
-      },
-    });
+    },
   });
+});
